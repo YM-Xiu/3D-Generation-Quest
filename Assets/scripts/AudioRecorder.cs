b732490859e90c786eb3327837f1a34c2a5dfdb3
Sen-26 <81495960+Sen-26@users.noreply.github.com>
2025-07-08 13:52:20 -0400

    crash issue fixed

--- Assets/scripts/AudioRecorder.cs
+++ Assets/scripts/AudioRecorder.cs
@@ -30,7 +30,7 @@ public class AudioRecorder : MonoBehaviour
     private IEnumerator StartRecordingRoutine()
     {
         isRecording = true;
-        recordedClip = Microphone.Start(null, false, 10, 44100); // Max 10 seconds duration !!!
+        recordedClip = Microphone.Start(null, false, 30, 44100); // Max 10 seconds duration !!!
         yield return null; // optional wait
     }
 
